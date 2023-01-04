1be93f7abf5576c8301066ba15b8cae1813f238c
juan pablo <juanpablo_1810dev@hotmail.com>
2023-01-04 12:00:35 -0500

    punto6

--- taller4.java
+++ taller4.java
@@ -47,8 +47,6 @@ public class taller4 {
 			System.out.print(i + ") "
 					+ "ELABORAR UN ALGORITMO QUE LE PERMITA AL USUARIO LEER 3 NÚMERO Y EL COMPUTADOR LE IMPRIMA EL MENOR DE ELLOS.");
 
-			
-
 			Scanner a = new Scanner(System.in);
 			System.out.println("\n Ingresa un numero");
 			int numero1 = a.nextInt();
@@ -62,15 +60,14 @@ public class taller4 {
 			int numero3 = c.nextInt();
 
 			if ((numero1 > numero3) && (numero1 > numero2)) {
-				System.out.print("\n El numero mayor es " + numero1 +"\n\n");
-				
+				System.out.print("\n El numero mayor es " + numero1 + "\n\n");
+
 			}
-			if ((numero2 > numero1 )&& (numero2 > numero3) ) {
-				System.out.print("\n El numero mayor es " + numero2 +"\n\n");
-				
-			}
-			 else {
-				System.out.print("\n El numero mayor es " + numero3 +"\n\n");
+			if ((numero2 > numero1) && (numero2 > numero3)) {
+				System.out.print("\n El numero mayor es " + numero2 + "\n\n");
+
+			} else {
+				System.out.print("\n El numero mayor es " + numero3 + "\n\n");
 			}
 
 			taller4.main(null);
@@ -96,27 +93,22 @@ public class taller4 {
 			Scanner d = new Scanner(System.in);
 			System.out.println("\n ingresa la nota 3");
 			double nota3 = d.nextDouble();
-                        
-                        Scanner e = new Scanner(System.in);
+
+			Scanner e = new Scanner(System.in);
 			System.out.println("\n ingresa la nota 4");
 			double nota4 = e.nextDouble();
 
-			double total= (nota1+nota2+nota3+nota4)/4;
-
-
+			double total = (nota1 + nota2 + nota3 + nota4) / 4;
 
 			if (total >= 3.0) {
-				System.out.print("\n Nombre:" +nestu+ "nota : " +total+" Gana "+ "\n \n");
+				System.out.print("\n Nombre:" + nestu + "nota : " + total + " Gana " + "\n \n");
 			}
-			if(total < 2.9 && total >= 2.0) 
-                        {
-				System.out.print("\n Nombre:" +nestu+ "nota : " +total+" Habilita "+ "\n \n");
-			}
-                        else if(total<1.9){
-				System.out.print("\n Nombre:" +nestu+ "nota : " +total+" Pierde "+ "\n \n");
+			if (total < 2.9 && total >= 2.0) {
+				System.out.print("\n Nombre:" + nestu + "nota : " + total + " Habilita " + "\n \n");
+			} else if (total < 1.9) {
+				System.out.print("\n Nombre:" + nestu + "nota : " + total + " Pierde " + "\n \n");
 			}
 
-			
 			taller4.main(null);
 
 		}
@@ -147,7 +139,127 @@ public class taller4 {
 			taller4.main(null);
 
 		}
-                if (i == 4) {
+		if (i == 4) {
+			System.out.print(i + ") "
+					+ "Se necesita un programa que diga si una persona es apta para un equipo de baloncesto o no, para que sea apto debe cumplir que si es hombre sea mayor de edad, que mida mas de 1.70 mts., que pese menos de 75 kg., o si es mujer que tenga más de 16 años,que mida como mínimo 1.70 y que pese como máximo 60 kg. Se debe leer el nombre, el sexo (F = femenino, M = masculino), la edad, la estatura y el peso.");
+
+			Scanner a = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese el nombre");
+			String nombre = a.next();
+
+			Scanner aa = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese la edad");
+			int edad = aa.nextInt();
+
+			Scanner p = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese el peso");
+			Double peso = p.nextDouble();
+
+			Scanner e = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese la estatura");
+			Double estatura = e.nextDouble();
+
+			Scanner s = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese el sexo: (F = femenino, M = masculino)");
+			String sexo = s.next();
+
+			if (edad <= 17 && sexo.equals("M") || sexo.equals("m")) {
+				System.out.print("\n " + "Debe ser myor de edad para estar en el equipo");
+
+			}
+			if (estatura <= 1.69 && sexo.equals("M") || sexo.equals("m")) {
+
+				System.out.print("\n " + "Debes medir 1,70 o mas para estar en el equipo ");
+			}
+			if (peso > 45 && sexo.equals("M") || sexo.equals("m")) {
+				System.out.print("\n " + "Debes pesar 45kg o menos para estar en el equipo ");
+			}
+			if (edad <= 16 && sexo.equals("F") || sexo.equals("f")) {
+				System.out.print("\n " + "Debe ser myor de edad para estar en el equipo");
+
+			}
+			if (estatura <= 1.69 && sexo.equals("F") || sexo.equals("f")) {
+
+				System.out.print("\n " + "Debes medir 1,70 o mas para estar en el equipo ");
+			}
+			if (peso > 60 && sexo.equals("F") || sexo.equals("f")) {
+				System.out.print("\n " + "Debes pesar 60kg o menos para estar en el equipo ");
+			} else {
+				System.out.print("\n " + nombre + "Felicitaciones puedes ser parte del equipo");
+			}
+
+			taller4.main(null);
+
+		}
+		if (i == 5)
+
+		{
+			System.out.print(i + ") "
+					+ "ELABORAR UN PROGRAMA QUE LE PERMITA A UN USUARIO INGRESAR EL NOMBRE DE UN TRABAJADOR, Y EL NÚMERO DE HORAS TRABAJADAS, SE PIDE QUE EL PROGRAMA LE IMPRIMA EL SALARIO BRUTO, LAS BONIFICACIONES, LAS DEDUCCIONES Y EL SALARIO NETO; TENIENDO EN CUENTA QUE LAS BONIFICACIONES SERÁN DE $20.000.OO SI TRABAJÓ COMO MÁXIMO 48 HORAS, DE $50.000.OO SI TRABAJO ENTRE 49 Y 58 HORAS Y DE $100.000 SI TRABAJÓ MÁS DE 58 HORAS. LAS DEDUCCIONES SON DE $10.000 SI EL SALARIO BÁSICO HORA ES MENOR DE $5.000.OO, DE $20.000.OO SI EL SALARIO BÁSICO HORA ES MAYOR DE $5.000.OO Y MENOR DE $8.000.OO Y DE $ 50.000.OO SI SU SALARIO BÁSICO HORA ES DE $8.000.OO O MAS.");
+
+			Scanner n = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese el nombre");
+			String nombre = n.next();
+
+			Scanner h = new Scanner(System.in);
+			System.out.println("\n " + "Ingrese las horas trabajadas");
+			int horast = h.nextInt();
+
+			Scanner b = new Scanner(System.in);
+			System.out.println("\n ");
+			String her = b.next();
+
+			Scanner bb = new Scanner(System.in);
+			System.out.println("\n ");
+			int ed = bb.nextInt();
+
+			if (ed > horast) {
+				System.out.print("\n e" + "");
+				System.out.print("\n :" + "" + "\n \n");
+			} else {
+				System.out.print("\n " + "");
+				System.out.print("\n " + "" + "\n \n");
+			}
+
+			taller4.main(null);
+
+		}
+		if (i == 6) {
+			System.out.print(i + ") "
+					+ "PARA LA MATERIA DE DESTREZAS SE DETERMINÓ CON LOS ESTUDIANTES QUE, SI LA NOTA DEL PRIMER QUIZ ERA MENOR QUE LA DEL SEGUNDO, SE SUSTITUÍA LA PRIMERA NOTA POR LA SEGUNDA. ELABORE UN ALGORITMO QUE LE PERMITA AL PROFESOR INGRESAR LAS 4 NOTAS QUE OBTUVO UN ALUMNO Y EL COMPUTADOR LE MUESTRE LA NOTA DEFINITIVA Y LA CALIFICACIÓN CUALITATIVA QUE ES: “E” SI ES MAYOR O IGUAL A 4.5, “S” SI ES MAYOR O IGUAL A 4.0 Y MENOR DE 4.5, “B” SI ES MAYOR O IGUAL A 3.5 Y MENOR DE 4.0, “A” SI ES MAYOR O IGUAL A 3.0 Y MENOR DE 3.5, “D” SI ES MAYOR O IGUAL A 2.0 Y MENOR DE 3.0 E “I” SI ES MENOR DE 2.0");
+
+			Scanner n = new Scanner(System.in);
+			System.out.println("\n Ingrese el nombre del estudiante");
+			String nombre = n.next();
+
+			Scanner n1 = new Scanner(System.in);
+			System.out.println("\n Ingrese la nota1");
+			int nota1 = n1.nextInt();
+
+			Scanner n2 = new Scanner(System.in);
+			System.out.println("\n ingrese la nota2");
+			int nota2 = n2.nextInt();
+
+			Scanner n3 = new Scanner(System.in);
+			System.out.println("\n ingrese la nota3");
+			int nota3 = n3.nextInt();
+
+			Scanner n4 = new Scanner(System.in);
+			System.out.println("\n ingrese la nota4");
+			int nota4 = n4.nextInt();
+
+			if (nota1 > nota2) {
+				nota2 = nota1;
+			}
+
+			double notafinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+			System.out.print("\n nombre: " + nombre + "nota final: " + notafinal);
+
+			taller4.main(null);
+
+		}
+		if (i == 6) {
 			System.out.print(i + ") "
 					+ "");
 
@@ -178,74 +290,10 @@ public class taller4 {
 			taller4.main(null);
 
 		}
-                if (i == 5) {
-			System.out.print(i + ") "
-					+ "");
-
-			Scanner a = new Scanner(System.in);
-			System.out.println("\n ");
-			String he = a.next();
-
-			Scanner aa = new Scanner(System.in);
-			System.out.println("\n ");
-			int e = aa.nextInt();
-
-			Scanner b = new Scanner(System.in);
-			System.out.println("\n ");
-			String her = b.next();
-
-			Scanner bb = new Scanner(System.in);
-			System.out.println("\n ");
-			int ed = bb.nextInt();
-
-			if (e > ed) {
-				System.out.print("\n e" + "");
-				System.out.print("\n :" + "" + "\n \n");
-			} else {
-				System.out.print("\n " + "");
-				System.out.print("\n " + "" + "\n \n");
-			}
-
-			taller4.main(null);
-
+		if (i == 0) {
+			System.out.print("Cerrando...");
+			System.exit(0);
 		}
-                if (i == 6) {
-			System.out.print(i + ") "
-					+ "");
 
-			Scanner a = new Scanner(System.in);
-			System.out.println("\n ");
-			String he = a.next();
-
-			Scanner aa = new Scanner(System.in);
-			System.out.println("\n ");
-			int e = aa.nextInt();
-
-			Scanner b = new Scanner(System.in);
-			System.out.println("\n ");
-			String her = b.next();
-
-			Scanner bb = new Scanner(System.in);
-			System.out.println("\n ");
-			int ed = bb.nextInt();
-
-			if (e > ed) {
-				System.out.print("\n e" + "");
-				System.out.print("\n :" + "" + "\n \n");
-			} else {
-				System.out.print("\n " + "");
-				System.out.print("\n " + "" + "\n \n");
-			}
-
-			taller4.main(null);
-
-		}
-                 if (i == 0) {
-            System.out.print("Cerrando...");
-            System.exit(0);
-        }
-                
-                
-                
 	}
 }
